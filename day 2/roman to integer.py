561c38cfc47e65bf00a27634a4ab2d28e26cbe15
Gouransh-1928 <gsalonia194@gmail.com>
2026-06-16 12:35:05 +0530

    Create roman to integer.py

---
+++ day 2/roman to integer.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            
+            
+#example
+#nums = [14, 11, 58, 78]
+#target = 69
+
+#result = twoSum(nums, target)
+#print(result)
